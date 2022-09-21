077ee3949af2cd48fa43b57907bc8e266d265554
Jerry <jerrykress.wsj@gmail.com>
2022-09-21 16:05:19 +0100

    Init

---
+++ build/CMakeFiles/Stonks.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Stonks.
+# This may be replaced when dependencies are built.
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jerry/Code/Stonks
+# Build directory: /home/jerry/Code/Stonks/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
